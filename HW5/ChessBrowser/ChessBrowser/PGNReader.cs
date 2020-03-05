5b29b0f990aeb85fec4c9de341f5afa70a5b150c
Will Frank <willfrank98@gmail.com>
2020-03-04 20:58:33 -0700

    hw5 keeps on going

--- HW5/ChessBrowser/ChessBrowser/PGNReader.cs
+++ HW5/ChessBrowser/ChessBrowser/PGNReader.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace ChessBrowser
@@ -14,66 +15,130 @@ namespace ChessBrowser
 
 		public void ReadPGN(string filepath)
 		{
+			// regex to get text between quotes
+			var rgx = new Regex("\"([^\"]*)\"");
+
+			// read in all lines of text
 			string[] lines = System.IO.File.ReadAllLines(filepath);
 
 			for (int i = 0; i < lines.Length; i++)
 			{
 				var currEvent = new Event();
 				var currGame = new Game();
-				var playerOne = new Player();
-				var playerTwo = new Player();
+				var whitePlayer = new Player();
+				var blackPlayer = new Player();
 				// read in event tags
-				while (!lines[i].Equals("\n"))
+				while (!lines[i].Equals(""))
 				{
 					// remove brackets
 					var line = lines[i].Substring(1, lines[i].Length - 2);
 					
 					if (line.StartsWith("Event")) 
 					{
-						currEvent.Name = line.Substring(7, line.Length - 2);
+						//currEvent.Name = line.Substring(7, line.Length - 9);
+						currEvent.Name = rgx.Match(line).Groups[1].Value;
 					}
 					else if (line.StartsWith("Site"))
 					{
-						currEvent.Site = line.Substring(7, line.Length - 2);
+						currEvent.Site = rgx.Match(line).Groups[1].Value;
 					}
 					else if (line.StartsWith("EventDate"))
 					{
-						currEvent.Date = line.Substring(7, line.Length - 2);
+						currEvent.Date = rgx.Match(line).Groups[1].Value;
 					}
 					else if (line.StartsWith("Round"))
 					{
-
+						currGame.Round = rgx.Match(line).Groups[1].Value;
 					}
 					else if (line.StartsWith("White"))
 					{
-
+						currGame.WhitePlayer = rgx.Match(line).Groups[1].Value;
+						whitePlayer.Name = rgx.Match(line).Groups[1].Value;
 					}
 					else if (line.StartsWith("Black"))
 					{
-
+						currGame.BlackPlayer = rgx.Match(line).Groups[1].Value;
+						blackPlayer.Name = rgx.Match(line).Groups[1].Value;
 					}
 					else if (line.StartsWith("Result"))
 					{
+						var tempRes = rgx.Match(line).Groups[1].Value;
+						char result = ' ';
+						switch (tempRes)
+						{
+							case "1/2-1/2":
+								result = 'D';
+								break;
+							case "0-1":
+								result = 'B';
+								break;
+							case "1-0":
+								result = 'W';
+								break;
+						}
 
+						currGame.Result = result;
 					}
 					else if (line.StartsWith("WhiteElo"))
 					{
-
+						whitePlayer.Elo = int.Parse(rgx.Match(line).Value);
 					}
 					else if (line.StartsWith("BlackElo"))
 					{
-
+						blackPlayer.Elo = int.Parse(rgx.Match(line).Value);
 					}
 					//else if (line.StartsWith("ECO"))
 					//{
 
 					//}
-					else if (line.StartsWith("Date"))
-					{
+					//else if (line.StartsWith("Date"))
+					//{
 
-					}
+					//}
+					i++;
+				}
+				i++;
+
+				// get moves
+				string moves = "";
+				while (!lines[i].Equals(""))
+				{
+					moves += lines[i];
+					i++;
+				}
+
+				currGame.Moves = moves;
+
+				events.Add(currEvent);
+				games.Add(currGame);
+
+				// check to see if whitePlayer already exist in current list and updates ELO if necessary
+				var tempPlayer = players.First(p => p.Name.Equals(whitePlayer.Name));
+				if (tempPlayer != null && tempPlayer.Elo < whitePlayer.Elo)
+				{
+					players.Where(p => p.Name.Equals(whitePlayer.Name)).First().Elo = whitePlayer.Elo;
+				}
+				else
+				{
+					players.Add(blackPlayer);
+				}
+
+				// does the same for blackPlayer
+				tempPlayer = players.First(p => p.Name.Equals(blackPlayer.Name));
+				if (tempPlayer != null && tempPlayer.Elo < blackPlayer.Elo)
+				{
+					players.Where(p => p.Name.Equals(blackPlayer.Name)).First().Elo = blackPlayer.Elo;
+				}
+				else
+				{
+					players.Add(blackPlayer);
 				}
 			}
+		}
+
+		public int GetNumWorkItems()
+		{
+			return games.Count + events.Count + players.Count;
 		}
 	}
 
